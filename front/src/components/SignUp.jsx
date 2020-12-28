c31c98d1a9ec83be30be9c0686b3b3f27bbcb94e
FabienNiepceron <fabienniepceron72@gmail.com>
2020-12-28 16:00:34 +0100

    niepceron_fabien_quetes020

--- front/src/components/SignUp.jsx
+++ front/src/components/SignUp.jsx
@@ -6,21 +6,57 @@ class SignUp extends React.Component {
         super()
     
         this.state={
-        email:"Email"
+        name:"",
+        lastname:"",
+        email:"",
+        password:"",
+        passworbis:"",
     }
 
         this.updateEmailField = this.updateEmailField.bind(this);
+        this.updateNameField = this.updateNameField.bind(this);
+        this.updateLastNameField = this.updateLastNameField.bind(this);
+        this.updatePasswordField = this.updatePasswordField.bind(this);
+        this.updatePasswordBisField = this.updatePasswordBisField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 }   
     updateEmailField = (e) => {
         this.setState({[e.target.name]: e.target.value})
     }
+
+    updateNameField = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    updateLastNameField = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    updatePasswordField = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    updatePasswordBisField = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state)
+    }
     
     render () {
         return(
+            <form onSubmit={this.handleSubmit}>
             <div>
-            <h1>{this.state.email} </h1>
+            <h1>Sign Up</h1>
+            <input type="text" name="name" value={this.state.name} onChange={this.updateNameField}/>
+            <input type="text" name="lastname" value={this.state.lastname} onChange={this.updateLastNameField}/>
             <input type="email" name="email" value={this.state.email} onChange={this.updateEmailField}/>
+            <input type="password" name="password" value={this.state.password} onChange={this.updatePasswordField}/>
+            <input type="password" name="passwordbis" value={this.state.passwordbis} onChange={this.updatePasswordBisField}/>
+            <input type="submit" value="Soumettre" />
             </div>
+            </form>
         ) 
     }
 }
